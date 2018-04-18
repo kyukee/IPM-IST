0d44ceb8fe84f090ba22d488b7ab43e86105f0ea
Unknown <moreira.oliveira@live.com.pt>
2018-04-18 15:23:03 +0100

    Added lineup data

--- Homepage/Prototypes/First_draft_improved/js/lineup.js
+++ Homepage/Prototypes/First_draft_improved/js/lineup.js
@@ -4,38 +4,180 @@ var lineupData = {
 	{
 		18:{
 			hora:"14:21:54",
-			palco:"secundario"
+			palco:"Secundario"
 		},
 
 		19:{
 			hora:"00:09:50",
-			palco:"sec"
+			palco:"Secundario"
 		}
 	},
 
 	"Alicia Keys":
 	{
 		18:{
-			hora:"",
-			palco:"prin"
+			hora:"19:25:05",
+			palco:"Principal"
 		},
 
 		20:{
-			hora:"",
-			palco:"tri"
+			hora:"18:13:54",
+			palco:"Terciario"
 		}
 	},
 
 	"Fallout Boy":
 	{
 		18:{
-			hora:"",
-			palco:"prin"
+			hora:"12:25:52",
+			palco:"Secundario"
 		},
 
 		19:{
-			hora:"",
-			palco:"sec"
+			hora:"23:15:20",
+			palco:"Terciario"
+		}
+	},
+
+	"Guns N Roses":
+	{
+		18:{
+			hora:"18:30:00",
+			palco:"Principal"
+		},
+
+		19:{
+			hora:"09:55:23",
+			palco:"Secundario"
+		}
+	},
+
+	"Hardwell":
+	{
+		18:{
+			hora:"17:05:30",
+			palco:"Principal"
+		},
+
+		19:{
+			hora:"22:26:05",
+			palco:"Secundario"
+		}
+	},
+
+	"Fallout Boy":
+	{
+		18:{
+			hora:"14:26:55",
+			palco:"Principal"
+		},
+
+		19:{
+			hora:"17:40:05",
+			palco:"Secundario"
+		}
+	},
+	"Marshmello":
+	{
+		18:{
+			hora:"14:20:00",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"12:55:03",
+			palco:"Secundario"
+		}
+	},
+
+	"Red Hot Chili Peppers":
+	{
+		18:{
+			hora:"12:02:05",
+			palco:"Principal"
+		},
+
+		19:{
+			hora:"07:55:02",
+			palco:"Secundario"
+		}
+	},
+
+	"The Who":
+	{
+		18:{
+			hora:"19:50:20",
+			palco:"Principal"
+		},
+
+		19:{
+			hora:"13:40:48",
+			palco:"Secundario"
+		}
+	},
+
+	"Oasis":
+	{
+		19:{
+			hora:"07:55:33",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"09:26:00",
+			palco:"Secundario"
+		}
+	},
+
+	"The Killers":
+	{
+		19:{
+			hora:"08:20:00",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"15:56:00",
+			palco:"Secundario"
+		}
+	},
+
+	"Ed Sheeran":
+	{
+		19:{
+			hora:"09:30:45",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"20:54:21",
+			palco:"Secundario"
+		}
+	},
+
+	"Mariah Carey":
+	{
+		19:{
+			hora:"14:55:54",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"10:55:03",
+			palco:"Secundario"
+		}
+	},
+
+	"Beyonce":
+	{
+		19:{
+			hora:"15:05:26",
+			palco:"Principal"
+		},
+
+		20:{
+			hora:"12:30:02",
+			palco:"Secundario"
 		}
 	}
 };
